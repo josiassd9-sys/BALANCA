10ae66f8c87e75294cdc2f9da83e332da09a49e4
Josias <josias.sd9@gmail.com>
2025-10-29 21:03:21 +0000

    Parece que resolveu esse problema. Agora poderia preparar esse app pra s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <meta name="theme-color" content="#212936" />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
