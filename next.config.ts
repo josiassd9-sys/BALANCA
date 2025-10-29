25f969f7129ea5a2994e409e4e7904b9fd5c603b
Josias <josias.sd9@gmail.com>
2025-10-29 01:05:36 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -37,7 +37,7 @@ const pwaConfig = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: !!process.env.TURBOPACK,
+  disable: process.env.NODE_ENV === 'development',
 });
 
 export default pwaConfig(nextConfig);
