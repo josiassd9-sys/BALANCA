f3e260c2d69cff99c4be174cadb32125a46a198a
Josias <josias.sd9@gmail.com>
2025-12-02 13:34:08 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'us

--- src/hooks/use-scale.ts
+++ src/hooks/use-scale.ts
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useEffect, useRef } from 'use-sync-external-store/shim';
+import { useState, useEffect, useRef } from 'react';
 import { io, Socket } from 'socket.io-client';
 
 export interface ScaleConfig {
