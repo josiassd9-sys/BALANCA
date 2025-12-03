92cb6aee9d16f8d431c7f9b3936cc48214a6307b
Josias <josias.sd9@gmail.com>
2025-12-03 13:29:26 +0000

    só uma pergunta, nessa pagina de previw do nosso aplicativo, ao mudar as

--- src/app/balanca/preview/page.tsx
+++ src/app/balanca/preview/page.tsx
@@ -4,7 +4,7 @@ import PrintableScaleTicket from "@/components/printable-scale-ticket";
 
 export default function PrintPreviewPage() {
   return (
-    <div className="bg-gray-800 p-8 min-h-screen">
+    <div className="bg-background p-8 min-h-screen">
         <div className="max-w-4xl mx-auto">
             <PrintableScaleTicket autoPrint={false} />
         </div>
